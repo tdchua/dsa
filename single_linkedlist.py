771e39013497b95831560d77216488b939d84c89
Timothy Joshua Chua <timothy.d.chua@gmail.com>
2020-08-13 19:53:27 +0800

    Added traversal function

--- single_linkedlist.py
+++ single_linkedlist.py
@@ -12,12 +12,32 @@ class SLinked_List:
   def __init__(self):
     self.head = None
 
+  def traverse(self):
+      curr_node = self.head
+      while(True):
+        if(curr_node != None):
+          print(curr_node.value)
+          curr_node = curr_node.next
+
+
+
 if __name__ == "__main__":
 
-  mylist = [i for i in range(10)]
+  my_list = [i for i in range(10)]
 
-  #Here is where we instantiate a list
+  #Here is where we instantiate a single linked list.
+  #We instantiate the head node first; we give it the first value
+  head = Node(my_list[0])
+  #We then instantiate the linked list
+  my_singly_linked_list = SLinked_List()
+  #We connect the head pointer of the linked list to the first node
+  my_singly_linked_list.head = head
 
+  for element in my_list[1:]:
+    next_node = Node(element)
+    head.next = next_node
+    head = next_node
 
-
-  print(mylist)
+  #We then traverse the link
+  curr_node = my_singly_linked_list.head
+  my_singly_linked_list.traverse()
