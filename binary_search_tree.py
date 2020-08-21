05785153165be2d6b8fb56ca880d18ff0fea62ec
Timothy Joshua Chua <timothy.d.chua@gmail.com>
2020-08-21 14:15:42 +0800

    Fixed insertion function and added preorder traversal

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -12,21 +12,38 @@ class BinarySearchTree: #The BST Class
   def __init__(self, root):
     self.root = root
 
+  #This insert method is for cases wherein there is no root yet.
   def insert(self, value):
     if(self.root == None):
       self.root = Node(value)
     else:
+      print("Inserting a Node!")
       self.insert_Node(self.root, value)
 
   def insert_Node(self, currentNode, value):
-    if(currentNode == None): #There is no root of the tree yet
-      print("Checking")
-      currentNode = Node(value)
-    else: #The tree has already been instantiated
-      if(value < currentNode.value): #Check the left side
+    if(value < currentNode.value): #Check the left side
+      if(currentNode.left == None):
+        currentNode.left = Node(value)
+      else:
         self.insert_Node(currentNode.left, value)
-      elif(value >= currentNode.value): #Check the right side
+    elif(value >= currentNode.value): #Check the right side
+      print("Bye", currentNode.value)
+      if(currentNode.right == None):
+        currentNode.right = Node(value)
+      else:
         self.insert_Node(currentNode.right, value)
+      # print(value)
+
+  def traversal(self, curr_node, mode=0): #mode = 0-Preorder, 1-Postorder, 2-BFS
+    if(mode == 0 or mode == None):
+        #Implementation of Preorder traversal
+        if(curr_node == None):
+          return
+        else:
+          print(curr_node.value)
+          # self.traversal(curr_node.left)
+          self.traversal(curr_node.right)
+
 
 
 if __name__ == "__main__":
@@ -39,5 +56,10 @@ if __name__ == "__main__":
 
 
   #Let's try printing the root
+  print("Root")
   print(my_BST.root)
   print(my_BST.root.value)
+
+  #Preorder Traversal!
+  print("Preorder Traversal")
+  my_BST.traversal(my_BST.root, 0)
